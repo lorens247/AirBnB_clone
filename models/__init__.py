deb9dd3f985bc3ec804283a6dfc46fd6ab7b52e9
Lawrence Ikhidero <lawrence@ikhidero.com>
2023-05-14 11:31:10 -0700

    updated

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Package handler"""
-from engine.file_storage import FileStorage
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
